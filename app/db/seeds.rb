86e35c81c91bda50da4db92faa16c4ccc0274d23
Bruno Campregher <brunocampregher99@gmail.com>
2024-05-16 22:18:37 -0300

    Update seed data

--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -1,54 +1,74 @@
 # db/seeds.rb
+
 require_relative '../models/user'
-require_relative '../models/question'
 require_relative '../models/option'
-require_relative '../models/progress'
+require_relative '../models/question'
 
 users = [
-  { username: 'jondoe', email: 'jon@doe.com', password: 'abc' },
-  { username: 'janedoe', email: 'jane@doe.com', password: 'abc' },
-  { username: 'babydoe', email: 'baby@doe.com', password: 'abc' },
+  { username: 'jondoe', email: 'jon@doe.com', password: 'abc', remaining_life_points: 100 },
+  { username: 'janedoe', email: 'jane@doe.com', password: 'abc', remaining_life_points: 100 },
+  { username: 'babydoe', email: 'baby@doe.com', password: 'abc', remaining_life_points: 100 }
 ]
 
-questions = [
-  { id: 1, description: 'Pregunta 1' },
-  { id: 2, description: 'Pregunta 2' }
-]
-
-# Falta relacionarlo con atributo del usuario
-progresses = [
-    { numberOfCorrectAnswers: 20, numberOfAchivements: 5, progressLevel: 'Capo' },
-    { numberOfCorrectAnswers: 1, numberOfAchivements: 0, progressLevel: 'Malardo' }
-]
-
-options = [
-  { id: 11, description: 'Opcion 1', correct: true},
-  { id: 12, description: 'Opcion 2', correct: false},
-  { id: 13, description: 'Opcion 3', correct: false},
-  { id: 14, description: 'Opcion 4', correct: false},
-]
-
-# Crear usuarios
 users.each do |u|
-  unless User.exists?(u[:email])
-    User.create(u)
-  end
+  User.create(u)
 end
 
-#HACER: modificar para que verifique existencia
-progresses.each do |p|
-    Progress.create(p)
-end
 
-# Crear preguntas
-questions.each do |q|
-  unless Question.exists?(q[:id])
-    Question.create(q)
-  end
-end
+questions_data = [
+  {
+    description: '¿Cuál es la forma correcta de asignar un valor a una variable en Python?',
+    options: [
+      { description: 'nombre de la variable = valor', value: true },
+      { description: 'valor = nombre de la variable', value: false },
+      { description: 'variable = valor', value: false },
+      { description: 'variable: valor', value: false }
+    ]
+  },
+  {
+    description: '¿Cómo se asigna un valor a una variable en Python?',
+    options: [
+      { description: 'Con el operador "=="', value: false },
+      { description: 'Utilizando corchetes "[ ]"', value: false },
+      { description: 'Utilizando el operador "="', value: true },
+      { description: 'Con el operador "+"', value: false }
+    ]
+  },
+  {
+    description: '¿Qué representan los valores booleanos en Python?',
+    options: [
+      { description: 'Números enteros', value: false },
+      { description: 'Verdadero o falso', value: true },
+      { description: 'Cadenas de texto', value: false },
+      { description: 'Valores negativos', value: false }
+    ]
+  },
+  {
+    description: '¿Cuál es el resultado de la siguiente operación en Python? resultado = (4 > 5) and (3 < 8)',
+    options: [
+      { description: 'True', value: false },
+      { description: 'False', value: true },
+      { description: '0', value: false },
+      { description: '1', value: false }
+    ]
+  },
+  {
+    description: '¿Cuál es el resultado de la siguiente operación en Python? resultado = 7 - 4 * 2',
+    options: [
+      { description: '14', value: false },
+      { description: '6', value: false },
+      { description: '-1', value: true },
+      { description: '0', value: false }
+    ]
+  }
+]
 
-options.each do |o|
-  unless Option.exists?(o[:id])
-    Option.create(o)
+
+
+questions_data.each do |question_data|
+  question = Question.create(description: question_data[:description])
+
+  question_data[:options].each do |option_data|
+    Option.create(description: option_data[:description], value: option_data[:value], question_id: question.id)
   end
 end
