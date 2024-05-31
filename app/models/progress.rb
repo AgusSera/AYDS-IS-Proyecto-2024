d01d410eb17881c6019b0ab3b147655ac4247b5a
Bruno Campregher <brunocampregher99@gmail.com>
2024-05-30 22:42:53 -0300

    advance_to_next_lesson method added in Progress class

--- app/models/progress.rb
+++ app/models/progress.rb
@@ -1,4 +1,9 @@
 class Progress < ActiveRecord::Base
   # relacion 1 a 1 con Progress en la tabla Progress
   has_one :user
+
+  def advance_to_next_lesson
+    self.current_lesson += 1
+    save
+  end
 end
