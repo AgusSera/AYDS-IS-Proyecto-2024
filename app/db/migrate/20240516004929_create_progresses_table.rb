898271f47ce0c3dd4cfdbb6ca04f227e73328282
Bruno Campregher <brunocampregher99@gmail.com>
2024-06-11 08:19:48 -0300

    Changes in ProgressTable

--- app/db/migrate/20240516004929_create_progresses_table.rb
+++ app/db/migrate/20240516004929_create_progresses_table.rb
@@ -4,12 +4,15 @@ class CreateProgressesTable < ActiveRecord::Migration[7.1]
   def change
     create_table :progresses do |t|
       t.integer :current_lesson, null: false, default: 1
-      t.integer :numberOfCorrectAnswers, null: false, default: 0
+      t.float :numberOfCorrectAnswers, null: false, default: 0
+      t.float :numberOfIncorrectAnswers, null: false, default: 0
       t.integer :numberOfAchivements, null: false, default: 0
       t.string :progressLevel, null: false, default: "Junior"
+      t.text    "correct_answered_questions", default: '[]'
 
       t.datetime :created_at
       t.datetime :updated_at
     end
   end
 end
+
