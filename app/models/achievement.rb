5cc435858a39a9e57904418448b11db4fa29eb62
Nicolas Centeno O. <nicocenteno202@gmail.com>
2024-06-13 18:30:07 -0300

    Achievement model and migration removed

--- app/models/achievement.rb
+++
@@ -1,4 +0,0 @@
-class Achievement < ActiveRecord::Base
-  # referencia a la relacion 1 a N entre User y Achievement
-  belongs_to :user
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,6 @@ class User < ActiveRecord::Base
   # referencia a la relacion N a N entre User y Question
   has_many :user_questions
   has_many :questions, through: :user_questions
-  # referencia a la relacion 1 a N entre User y Achievement
-  has_many :achievements
   # CF de la relacion 1 a 1 con Progress en la tabla Progress
   belongs_to :progress
   # TODO? relacion con Ranking
